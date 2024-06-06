08b03a7194e6e6880844e76208a6a9841d58f5f3
stefanpopovic <stefan.popovic@tnation.eu>
2024-06-06 21:33:41 +0200

    Add project files.

---
+++ Shop.Presentation/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Shop.Presentation
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {           
+            return services;
+        }
+    }
+}
